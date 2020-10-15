75f8d6aae411d306037651dbb16a817b25eb3381
Manoj Bhadugale <manojbhadugale@hotmail.com>
2020-10-15 17:50:04 +0530

    Initial commit

---
+++ Elements/Node.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgramming.Elements
+{
+    public class Node
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public List<Property> Inputs = new List<Property>();
+        public List<Property> Outputs = new List<Property>();
+        public bool Enabled { get; set; } = true;
+
+        public virtual void Update() { }
+
+        public virtual void Draw(Graphics g)
+        {
+
+        }
+    }
+
+    public class ValueNode : Node
+    {
+        public ValueNode()
+        {
+        }
+
+        public override void Update()
+        {
+
+        }
+    }
+
+    public class IntValueNode : ValueNode
+    {
+
+    }
+}
---
+++ Elements/Property.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualProgramming.Elements
+{
+    public abstract class Property
+    {
+        Node containernode;
+
+        public Property(Node containernode)
+        {
+            this.containernode = containernode;
+        }
+
+        public Node ContainerNode { get; }
+        public string Name { get; set; }
+        public abstract object Value { get; set; }
+    }
+
+    public class IntProperty : Property
+    {
+        public IntProperty(Node containernode) : base(containernode) { }
+        int value;
+        public override object Value
+        {
+            get => value;
+            set => this.value = (int)value;
+        }
+    }
+    public class StringProperty : Property
+    {
+        public StringProperty(Node containernode) : base(containernode) { }
+
+        string value;
+        public override object Value
+        {
+            get => value;
+            set => this.value = (string)value;
+        }
+    }
+
+    public class ArrayProperty : Property
+    {
+        public ArrayProperty(Node containernode) : base(containernode) { }
+
+        List<object> value;
+        public override object Value
+        {
+            get => value;
+            set => this.value = (List<object>)value;
+        }
+    }
+}
